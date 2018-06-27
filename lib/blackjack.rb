6ee31dc415ce86369626a0410cf233691479dc97
Juliana Kang <julianajlk@gmail.com>
2018-06-27 23:09:28 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,6 @@ end
 def initial_round
   # code #initial_round here
   current_card_total = deal_card
-  
   card_total = deal_card + deal_card
   display_card_total(card_total)
   return card_total
