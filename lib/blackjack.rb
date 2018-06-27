c353ca379510fb9f192e642e5cccf77ebfd394e1
Juliana Kang <julianajlk@gmail.com>
2018-06-27 23:03:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,8 +57,8 @@ end
 def runner
   # code runner here
   welcome
-  initial_round = card_total
-  while card_total < 21
+  card_total = initial_round
+  until card_total > 21
     card_total = hit?(card_total)
     card_total = display_card_total(card_total)
   end
